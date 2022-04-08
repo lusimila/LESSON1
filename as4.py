1779c26eecb635903b2487dbea48d234f592c0b4
lusimila <melodia.mila@mail.ru>
2022-04-08 21:16:46 +0300

    com3

---
+++ as4.py
@@ -0,0 +1,34 @@
+# def sum_namb(ls_namb):
+#     return sum(ls_namb)
+# print(sum_namb([5, 4, 7]))
+
+# def muit_1(ls):
+#     res = 1
+#     for i in ls:
+#         res *= i
+#     return res
+# print(muit_1([2,4,6]))
+
+# Создайте функцию count_letters,
+# которая принимает на вход фразу и
+# подсчитывает, какое количество в ней
+# строчных(down) и заглавных (up) букв.
+# Функция должна вывести на
+
+def count_letter(st_1):
+    a = 0
+    b = 0
+    for i in st_1:
+        if i isalpa() and i.islolower():
+            down += 1
+        eiif i.isalpa() and i.isupper():
+        
+
+
+
+
+
+
+
+
+
