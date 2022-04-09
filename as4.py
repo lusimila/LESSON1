d76943dc3731a09e0ee8c4a5bfad28ef7492e5d3
lusimila <melodia.mila@mail.ru>
2022-04-09 23:57:13 +0300

    str_1

--- as4.py
+++ as4.py
@@ -15,14 +15,22 @@
 # строчных(down) и заглавных (up) букв.
 # Функция должна вывести на
 
-def count_letter(st_1):
-    a = 0
-    b = 0
-    for i in st_1:
-        if i isalpa() and i.islolower():
-            down += 1
-        eiif i.isalpa() and i.isupper():
-        
+# def count_letter(st_1):
+#     a = 0
+#     b = 0
+#     for i in st_1:
+#         if i isalpa() and i.islolower():
+#             down += 1
+#         eiif i.isalpa() and i.isupper():
+
+# Напишите программу ввода двух слов  через  пробел.Сформируйте новую строку,
+# продублировав первое слово дважды, а второе - трижды (все слова  в  результирующей
+# строке должны идти через пробел).Результат выведите на экран. Программу следует
+# реализовать без использования F - строк, а с применением оператора дублирования строк.
+# hello python    hello hello python python python
+s1, s2 = map(str.strip, input().split())
+print((s1+" ")*2, (s2+" ")*3, sep= "")
+
 
 
 
